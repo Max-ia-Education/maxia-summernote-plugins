ff6fb7d5eb10c9c51eba8e19bf6fd48f0a1a835f
Eduardo <eduardorodriguesanjos@gmail.com>
2023-07-14 11:45:43 -0300

    [feat] first working version of summernote math

--- summernote-math.js
+++ summernote-math.js
@@ -108,23 +108,38 @@
                 // Math virtual keyboard personalization
                 document.body.style.setProperty("--keyboard-zindex", "1051");
                 document.querySelector('math-field').addEventListener('focus', () => {
+                    // mathVirtualKeyboard.layouts = ["numeric", "symbols"];
+                    mathVirtualKeyboard.layouts = [
+                        {
+                            label: "Básico",
+                            toolip: "Only the essential",
+                            style: ".digit { background: blue; color: white }",
+                            rows: [
+                                ["[+]", "[-]", "[*]", "[/]", "[=]", "[.]", "[(]", "[)]", "\\sqrt{#0}", "#@^{#?}"],
+                                ["[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]", "[0]"],
+                                ["[hr]"], ["[shift]", "[undo]", "[redo]", "[separator]", "[separator]", "[separator]", "[left]", "[right]", 
+                                {
+                                    label: "[backspace]",
+                                    class: "action hide-shift"
+                                }, 
+                                "[hide-keyboard]"]]
+                          }, 
+                          "greek"
+                        ];
                     mathVirtualKeyboard.visible = true;
                 });
             }
 
             self.hasMath = function (node) {
-                console.log("hasMath")
                 return node && $(node).hasClass("note-math")
             }
 
             self.isOnMath = function (range) {
-                console.log("isOnMath")
                 const ancestor = $.summernote.dom.ancestor(range.sc, self.hasMath)
                 return !!ancestor && ancestor === $.summernote.dom.ancestor(range.ec, self.hasMath)
             }
 
             self.update = function () {
-                console.log('update')
                 // Prevent focusing on editable when invoke('code') is executed
                 if (!context.invoke("editor.hasFocus")) {
                     self.hide()
@@ -135,7 +150,6 @@
                 if (rng.isCollapsed() && self.isOnMath(rng)) {
                     const node = $.summernote.dom.ancestor(rng.sc, self.hasMath)
                     const latex = $(node).find(".note-latex")
-                    console.log("🚀 ~ file: summernote-math.js:138 ~ latex:", latex)
 
                     if (latex.text().length !== 0) {
                         self.$popover.find("button").html(latex.text())
@@ -154,12 +168,10 @@
             }
 
             self.hide = function () {
-                console.log("hide")
                 self.$popover.hide()
             }
 
             self.destroy = function () {
-                console.log("destroy")
                 ui.hideDialog(this.$dialog)
                 self.$dialog.remove()
                 self.$popover.remove()
@@ -172,7 +184,6 @@
             }
 
             self.bindLabels = function () {
-                console.log("bindLabels")
                 self.$dialog.find(".form-control:first").focus().select()
                 self.$dialog.find("label").on("click", function () {
                     $(this).parent().find(".form-control:first").focus()
@@ -180,50 +191,39 @@
             }
 
             self.show = function () {
-                console.log("show")
                 let $mathSpan = self.$dialog.find(".note-math-dialog")
-                console.log("🚀 ~ file: summernote-math.js:196 ~ $mathSpan:", $mathSpan)
                 let $latexSpan = self.$dialog.find(".note-latex")
-                console.log("🚀 ~ file: summernote-math.js:198 ~ $latexSpan:", $latexSpan)
-                let $selectedMathNode = self.getSelectedMath()
-                console.log("🚀 ~ file: summernote-math.js:200 ~ $selectedMathNode:", $selectedMathNode)
+                let { $latexImg, latexString } = self.getSelectedMath()
 
-                if (!$selectedMathNode) {
+                if (!latexString) {
                     // reset the dialog input and math
                     $mathSpan.empty();
                     $latexSpan.val("")
                 } else {
                     // edit the selected math node
                     // get the hidden LaTeX markup from the selected math node
-                    let hiddenLatex = $selectedMathNode.find(".note-latex").text()
-                    $latexSpan.val(hiddenLatex)
+                    // let hiddenLatex = $selectedMathNode.find(".note-latex").text()
+                    $latexSpan.val(latexString)
                 }
 
                 let mathInfo = {} // not used
 
                 self.showMathDialog(mathInfo).then(function (mathInfo) {
-                    console.log("Insert the text")
                     ui.hideDialog(self.$dialog)
                     let $mathNodeClone = $latexSpan.clone() // $mathSpan.clone()
                     
                     // newEl.prepend($mathNodeClone[0])
-                    const ce = new ComputeEngine.ComputeEngine();
-                    let newEl = $('<img>').attr("src", `https://math.vercel.app?from=${ce.parse($latexSpan.val()).latex}`)//.addClass("note-math")
+                    const encodedLatex = encodeURIComponent($latexSpan.val().replaceAll('\\imaginaryI', "i"))
+                        .replace(/\(/g, "%28")
+                        .replace(/\)/g, "%29") + ".svg"
+                    let newEl = $('<img>').attr("src", `https://math.vercel.app?from=${encodedLatex}&originalLatex=${$latexSpan.val()}`)
                     
-                    newEl.prepend($mathSpan[0])
-                    console.log("🚀 ~ file: summernote-math.js:209 ~ newEl:", newEl)
-
-
                     // Add read-only attribute
                     // $mathNodeClone[0].readOnly = true;
-                    $mathNodeClone[0].style = ''
-                    $mathNodeClone[0].value = $latexSpan[0].value
-                    console.log("🚀 ~ file: summernote-math.js:207 ~ $mathSpan:", $mathSpan)
-                    console.log("🚀 ~ file: summernote-math.js:207 ~ $mathNodeClone:", $mathNodeClone)
-                    let $latexNode = $("<span>")
-                    $latexNode.addClass("note-latex").css("display", "none").text($latexSpan.val()).appendTo($mathNodeClone)
-
-                    console.log("🚀 ~ file: summernote-math.js:209 ~ newEl:", newEl)
+                    // $mathNodeClone[0].style = ''
+                    // $mathNodeClone[0].value = $latexSpan[0].value
+                    // let $latexNode = $("<span>")
+                    // $latexNode.addClass("note-latex").css("display", "none").text($latexSpan.val()).appendTo($mathNodeClone)
 
                     // So we don't pick up the dialog node when selecting math nodes in the editor
                     $mathNodeClone.removeClass("note-math-dialog").addClass("note-math")
@@ -232,22 +232,15 @@
                     context.invoke("editor.restoreRange")
                     context.invoke("editor.focus")
 
-                    console.log("🚀 ~ file: summernote-math.js:218 ~ $mathNodeClone:", $mathNodeClone)
-                    if ($selectedMathNode === null) context.invoke("editor.insertNode", newEl[0])
+                    if (latexString === null) context.invoke("editor.insertNode", newEl[0])
                     else {
                         // if we are editing an existing mathNode, just replace the contents:
-                        if ($.trim($latexNode.html()) == "") {
-                            // unless there's nothing there, then delete the node
-                            $selectedMathNode.remove()
-                        } else {
-                            $selectedMathNode.html($mathNodeClone.html())
-                        }
+                        $latexImg[0].replaceWith(newEl[0])
                     }
                 })
             }
 
             self.showMathDialog = function (editorInfo) {
-                console.log("showMathDialog")
                 return $.Deferred(function (deferred) {
                     let $editBtn = self.$dialog.find(".note-math-btn")
                     ui.onDialogShown(self.$dialog, function () {
@@ -268,39 +261,21 @@
             }
 
             self.getSelectedMath = function () {
-                console.log("getSelectedMath")
                 let selection = null
                 try {
                     selection = window.getSelection()?.getRangeAt(0)?.endContainer
                 } catch (error) {
-                    return null
+                    return {$latexImg: null, latexString: null}
                 }
                 
                 if (selection) {
-                    let selectedMathNode = null
-                    
-                    if (selection.className == "note-math") {
-                        selectedMathNode = selection
-                    } else {
-                        // Verify if selection is child of any .note-math
-                        let $mathParent = $(selection).parents(".note-math")
-                        if ($mathParent.length > 0) {
-                            selectedMathNode = $mathParent[0]
-                        }
-                    }
-                    console.log("🚀 ~ file: summernote-math.js:256 ~ selectedMathNode:", selectedMathNode)
-                    // // get all math nodes
-                    // let $mathNodes = $(selection).children(".note-math")
-                    // // let $mathNodes = $(".note-math")
-                    // $mathNodes.each(function () {
-                    //     console.log("Found math node:", this)
-                    //     // grab first math node in the selection (including partial).
-                    //     // if (selection.containsNode(this, true)) {
-                    //     selectedMathNode = $(this)
-                    //     // selection = $(this)
-                    //     // }
-                    // })
-                    return selectedMathNode ? $(selectedMathNode) : null
+                    let $latexImg = $(selection).find('img')
+                    if (!$latexImg[0]) return {$latexImg: null, latexString: null}
+
+                    let src = $latexImg.attr('src')
+                    let latexString = src.split('originalLatex=')[1]
+
+                    return {$latexImg, latexString}
                 }
             }
         },
--- summernote-options.js
+++ summernote-options.js
@@ -1,95 +1,3 @@
-window.initializeSummernote = function () {
-  var FontStyleButton = function (context) {
-    var ui = $.summernote.ui;
-    var options = $.summernote.options;
-
-    const bold = ui.button({
-      className: 'note-btn-bold',
-      contents: ui.icon(options.icons.bold),
-      click: context.createInvokeHandlerAndUpdateState('editor.bold')
-    })
-    const italic = ui.button({
-      className: 'note-btn-italic',
-      contents: ui.icon(options.icons.italic),
-      click: context.createInvokeHandlerAndUpdateState('editor.italic')
-    })
-    const underline = ui.button({
-      className: 'note-btn-underline',
-      contents: ui.icon(options.icons.underline),
-      click: context.createInvokeHandlerAndUpdateState('editor.underline')
-    })
-    const clear = ui.button({
-      contents: ui.icon(options.icons.eraser),
-      click: context.createInvokeHandler('editor.removeFormat')
-    })
-
-    const superscript = ui.button({
-      className: 'note-btn-superscript',
-      contents: ui.icon(options.icons.superscript),
-      click: context.createInvokeHandlerAndUpdateState('editor.superscript')
-    })
-
-    const subscript = ui.button({
-      className: 'note-btn-subscript',
-      contents: ui.icon(options.icons.subscript),
-      click: context.createInvokeHandlerAndUpdateState('editor.subscript')
-    });
-
-    // create button
-    let button = ui.buttonGroup([
-      ui.button({
-        className: 'dropdown-toggle',
-        contents: ui.dropdownButtonContents(ui.icon(options.icons.font), options),
-        tooltip: 'Estilo do texto',
-        data: {
-          toggle: 'dropdown'
-        }
-      }),
-      ui.dropdown([ui.buttonGroup({
-        className: 'note-align',
-        children: [bold, italic, underline, superscript, subscript, clear]
-      })])
-    ])
-
-    return button.render();   // return button as jquery object
-  }
-
-  $(".summernote").summernote({
-    inheritPlaceholder: true,
-    //essa parte é a api que renderiza a barra do summernote
-    toolbar: [
-      ['style', ['style', 'fontStyle']],
-      ['font', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'clear']],
-      ['table', ['table']],
-      ['para', ['ul', 'ol', 'paragraph']],
-      ['insert', ['picture', 'math']],
-      ["misc", ['undo', 'redo']]
-    ],
-    styleTags: [
-      { title: 'Título', tag: 'h2', value: 'h2' },
-      { title: 'Texto', tag: 'p', value: 'p' },
-      { title: 'Legenda', tag: 'h6', value: 'h6' }
-    ],
-
-    buttons: {
-      fontStyle: FontStyleButton
-    },
-    icons: {
-      magic: 'note-icon-pencil'
-    },
-    // Don't let the user clear all the tags (allways be in <p>, <h2> or <h6>)
-    callbacks: {
-      onChange: function(contents) {
-        if (contents.length === 0) {
-          $(this).summernote('undo')
-        }
-        $(this)[0].onchange()
-      }
-    }
-  })
-
-}
-
 window.FontStyleButton = function (context) {
   var ui = $.summernote.ui;
   var options = $.summernote.options;
@@ -145,6 +53,23 @@ window.FontStyleButton = function (context) {
   return button.render();   // return button as jquery object
 }
 
+// window.EditMathButton = function (context) {
+//   var ui = $.summernote.ui;
+//   var options = $.summernote.options;
+
+//   let button = ui.button({
+//     contents: options.math.icon,
+//     // container: false,
+//     tooltip: 'Editar equação',
+//     click: function (e) {
+//         // Cursor position must be saved because is lost when popup is opened.
+//         context.invoke("editor.saveRange")
+//         context.invoke("math.show")
+//     },
+//   })
+//   return button.render()
+// }
+
 window.summernoteOptions = {
   inheritPlaceholder: true,
   //essa parte é a api que renderiza a barra do summernote
@@ -162,8 +87,15 @@ window.summernoteOptions = {
     { title: 'Legenda', tag: 'h6', value: 'h6' }
   ],
 
+  popover: {
+    image: [
+      ['image', ['resizeHalf', 'resizeQuarter', 'math', 'resizeNone']],
+      ['remove', ['removeMedia']]
+    ]
+  },
   buttons: {
-    fontStyle: FontStyleButton
+    fontStyle: window.FontStyleButton,
+    editMath: window.EditMathButton
   },
   icons: {
     magic: 'note-icon-pencil'
@@ -174,7 +106,11 @@ window.summernoteOptions = {
       if (contents.length === 0) {
         $(this).summernote('undo')
       }
-      $(this)[0].onchange()
+      $(this)[0].onchange && $(this)[0].onchange()
     }
   }
+}
+
+window.initializeSummernote = function () {
+  $(".summernote").summernote(window.summernoteOptions)
 }
