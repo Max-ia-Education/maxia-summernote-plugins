8802e7e7ac0d23018d17fd4847009db84aa41ec6
Eduardo <eduardorodriguesanjos@gmail.com>
2023-07-14 07:23:24 -0300

    [feat] expose summernote otions on window object

--- summernote-options.js
+++ summernote-options.js
@@ -63,7 +63,7 @@ window.initializeSummernote = function () {
       ['table', ['table']],
       ['para', ['ul', 'ol', 'paragraph']],
       ['insert', ['picture', 'math']],
-      ["misc", ['undo', 'redo', 'codeview']]
+      ["misc", ['undo', 'redo']]
     ],
     styleTags: [
       { title: 'Título', tag: 'h2', value: 'h2' },
@@ -143,4 +143,38 @@ window.FontStyleButton = function (context) {
   ])
 
   return button.render();   // return button as jquery object
+}
+
+window.summernoteOptions = {
+  inheritPlaceholder: true,
+  //essa parte é a api que renderiza a barra do summernote
+  toolbar: [
+    ['style', ['style', 'fontStyle']],
+    ['font', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'clear']],
+    ['table', ['table']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['insert', ['picture', 'math']],
+    ["misc", ['undo', 'redo']]
+  ],
+  styleTags: [
+    { title: 'Título', tag: 'h2', value: 'h2' },
+    { title: 'Texto', tag: 'p', value: 'p' },
+    { title: 'Legenda', tag: 'h6', value: 'h6' }
+  ],
+
+  buttons: {
+    fontStyle: FontStyleButton
+  },
+  icons: {
+    magic: 'note-icon-pencil'
+  },
+  // Don't let the user clear all the tags (allways be in <p>, <h2> or <h6>)
+  callbacks: {
+    onChange: function(contents) {
+      if (contents.length === 0) {
+        $(this).summernote('undo')
+      }
+      $(this)[0].onchange()
+    }
+  }
 }
